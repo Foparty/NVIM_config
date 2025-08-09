5f1f965248f195d5a6435ac9c9fb634a961443e6
fo <103907142+Foparty@users.noreply.github.com>
2025-08-09 22:56:58 +0100

    using and config fzf
    i will use this because it can grep huge codebases

--- lua/custom/plugins/fzf-lua.lua
+++ lua/custom/plugins/fzf-lua.lua
@@ -1,59 +1,246 @@
 return {
 	"ibhagwan/fzf-lua",
-	enabled = false,
-	config = function()
-		require("fzf-lua").setup({
+	enabled = true,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = function(_, opts)
+		local actions = require("fzf-lua").actions
+		return {
+			keymap = {
+				fzf = {
+					["ctrl-d"] = "preview-page-down", -- Keep default
+					["ctrl-u"] = "preview-page-up", -- Keep default
+					["ctrl-t"] = "toggle-preview", -- Override/add
+				},
+			},
 			winopts = {
-				width = 0.95,
-				height = 0.95,
+				width = 0.95, -- Override default
+				height = 0.95, -- Override default
 			},
-			files = {
-				prompt = "Files❯ ",
-				cwd_prompt = true,
+			actions = {
+				files = {
+					["ctrl-q"] = actions.file_sel_to_qf, -- Add new action
+					["ctrl-i"] = actions.toggle_ignore,
+					["ctrl-h"] = actions.toggle_hidden,
+				},
 			},
-			grep = {
-				prompt = "Grep❯ ",
-			},
-			buffers = {
-				prompt = "Buffers❯ ",
-			},
-			keymaps = {
-				prompt = "Keymaps❯ ",
-			},
-			help = {
-				prompt = "Help❯ ",
-			},
-		})
-
-		local fzf = require("fzf-lua")
-
-		-- Keymaps
-		vim.keymap.set("n", "<leader><leader>", function()
-			fzf.files({ cwd = vim.loop.cwd() })
-		end, { desc = "Find Files" })
-		vim.keymap.set("n", "<leader>,", fzf.buffers, { desc = "Buffers" })
-		vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "Keymaps" })
-		vim.keymap.set("n", "<leader>/", fzf.live_grep, { desc = "Grep" })
-		vim.keymap.set("n", "<leader>fr", fzf.registers, { desc = "Registers" })
-		vim.keymap.set("n", "<leader>fc", function()
-			fzf.files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "Find Config File" })
-		vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help" })
-		vim.keymap.set("n", "<leader>fn", function()
-			fzf.files({ cwd = "~/notes/" })
-		end, { desc = "Find Notes" })
-		vim.keymap.set("n", "<leader>fm", function()
-			fzf.live_grep({ cwd = "~/notes/" })
-		end, { desc = "Grep Notes" })
-		vim.keymap.set({ "n", "x" }, "<leader>fw", fzf.grep_cword, { desc = "Grep word/selection" })
-
-		-- LSP
-		vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "Goto Definition" })
-		vim.keymap.set("n", "gD", fzf.lsp_declarations, { desc = "Goto Declaration" })
-		vim.keymap.set("n", "grr", fzf.lsp_references, { desc = "References" })
-		vim.keymap.set("n", "gI", fzf.lsp_implementations, { desc = "Goto Implementation" })
-		vim.keymap.set("n", "gy", fzf.lsp_typedefs, { desc = "Goto Type Definition" })
-		vim.keymap.set("n", "gs", fzf.lsp_document_symbols, { desc = "LSP Symbols" })
-		vim.keymap.set("n", "<leader>sS", fzf.lsp_workspace_symbols, { desc = "LSP Workspace Symbols" })
+		}
 	end,
+	keys = {
+		{
+			"<leader><leader>",
+			function()
+				require("fzf-lua").files({ cwd_only = true })
+			end,
+			desc = "files in cwd",
+		},
+		{
+			"<leader>.",
+			function()
+				require("fzf-lua").lsp_document_symbols()
+			end,
+			desc = "branches",
+		},
+		{
+			"<leader>,",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "open buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				require("fzf-lua").live_grep({ cwd_only = true })
+			end,
+			desc = "grep cwd",
+		},
+		{
+			"<leader>w",
+			function()
+				require("fzf-lua").grep_cword({ cwd_only = true, prompt = "current WORD" })
+			end,
+			desc = "grep cwd",
+		},
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "files in cwd",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "files in cwd",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("fzf-lua").helptags()
+			end,
+			desc = "help",
+		},
+		{
+			"<leader>fk",
+			function()
+				require("fzf-lua").keymaps()
+			end,
+			desc = "keymaps",
+		},
+		{
+			"<leader>gf",
+			function()
+				require("fzf-lua").git_bcommits()
+			end,
+			desc = "buffer commits",
+		},
+		{
+			"<leader>gb",
+			function()
+				require("fzf-lua").git_branches()
+			end,
+			desc = "branches",
+		},
+		{
+			"gs",
+			function()
+				require("fzf-lua").lsp_workspace_symbols()
+			end,
+			desc = "branches",
+		},
+		{
+			"gx",
+			function()
+				require("fzf-lua").diagnostics_workspace()
+			end,
+			desc = "branches",
+		},
+		{
+			"gd",
+			function()
+				require("fzf-lua").lsp_definitions()
+			end,
+			desc = "branches",
+		},
+		{
+			"gD",
+			function()
+				require("fzf-lua").lsp_declarations()
+			end,
+			desc = "references",
+		},
+		{
+			"gy",
+			function()
+				require("fzf-lua").lsp_typedefs()
+			end,
+			desc = "type definitions",
+		},
+		{
+			"gI",
+			function()
+				require("fzf-lua").lsp_implementations()
+			end,
+			desc = "implementations",
+		},
+		{
+			"g.",
+			function()
+				require("fzf-lua").lsp_code_actions()
+			end,
+			desc = "code actions",
+		},
+		{
+			"gm",
+			function()
+				require("fzf-lua").marks()
+			end,
+			desc = "marks",
+		},
+		{
+			"<leader>sc",
+			function()
+				require("fzf-lua").changes()
+			end,
+			desc = "changes",
+		},
+
+		{
+			"<leader>fr",
+			function()
+				require("fzf-lua").registers()
+			end,
+			desc = "registers",
+		},
+		{
+			"<leader>ss",
+			function()
+				require("fzf-lua").spellcheck()
+			end,
+			desc = "spellcheck",
+		},
+		{
+			"z=",
+			function()
+				require("fzf-lua").spell_suggest()
+			end,
+			desc = "spellcheck",
+		},
+	},
+	-- config = function()
+	--   require("fzf-lua").setup({
+	--     winopts = {
+	--       width = 0.95,
+	--       height = 0.95,
+	--     },
+	--     files = {
+	--       prompt = "Files❯ ",
+	--       cwd_prompt = true,
+	--     },
+	--     grep = {
+	--       prompt = "Grep❯ ",
+	--     },
+	--     buffers = {
+	--       prompt = "Buffers❯ ",
+	--     },
+	--     keymaps = {
+	--       prompt = "Keymaps❯ ",
+	--     },
+	--     help = {
+	--       prompt = "Help❯ ",
+	--     },
+	--   })
+	--
+	--
+	--   -- Keymaps
+	--   vim.keymap.set("n", "<leader><leader>", function()
+	--     fzf.files({ cwd = vim.loop.cwd() })
+	--   end, { desc = "Find Files" })
+	--   vim.keymap.set("n", "<leader>,", fzf.buffers, { desc = "Buffers" })
+	--   vim.keymap.set("n", "<leader>fk", fzf.keymaps, { desc = "Keymaps" })
+	--   vim.keymap.set("n", "<leader>/", fzf.live_grep, { desc = "Grep" })
+	--   vim.keymap.set("n", "<leader>fr", fzf.registers, { desc = "Registers" })
+	--   vim.keymap.set("n", "<leader>fc", function()
+	--     fzf.files({ cwd = vim.fn.stdpath("config") })
+	--   end, { desc = "Find Config File" })
+	--   vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help" })
+	--   vim.keymap.set("n", "<leader>fn", function()
+	--     fzf.files({ cwd = "~/notes/" })
+	--   end, { desc = "Find Notes" })
+	--   vim.keymap.set("n", "<leader>fm", function()
+	--     fzf.live_grep({ cwd = "~/notes/" })
+	--   end, { desc = "Grep Notes" })
+	--   vim.keymap.set({ "n", "x" }, "<leader>fw", fzf.grep_cword, { desc = "Grep word/selection" })
+	--
+	--   -- LSP
+	--   vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "Goto Definition" })
+	--   vim.keymap.set("n", "gD", fzf.lsp_declarations, { desc = "Goto Declaration" })
+	--   vim.keymap.set("n", "grr", fzf.lsp_references, { desc = "References" })
+	--   vim.keymap.set("n", "gI", fzf.lsp_implementations, { desc = "Goto Implementation" })
+	--   vim.keymap.set("n", "gy", fzf.lsp_typedefs, { desc = "Goto Type Definition" })
+	--   vim.keymap.set("n", "gs", fzf.lsp_document_symbols, { desc = "LSP Symbols" })
+	--   vim.keymap.set("n", "<leader>sS", fzf.lsp_workspace_symbols, { desc = "LSP Workspace Symbols" })
+	-- end,
 }
