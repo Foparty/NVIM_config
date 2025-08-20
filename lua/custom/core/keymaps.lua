3d2c4a42cae4960a3b4252ac4b44299beba1337a
fo <103907142+Foparty@users.noreply.github.com>
2025-08-20 22:31:47 +0100

    tmux plugin setup, had to comment my own mappings for splits navigations

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -31,29 +31,29 @@ map.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected lines up" })
 map.set("n", "<leader>]", "<cmd>cnext<CR>", { desc = "Go to next quickfix item" })
 map.set("n", "<leader>[", "<cmd>cprev<CR>", { desc = "Go to previous quickfix item" })
 
-map.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
-map.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
-map.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
-map.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+-- map.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
+-- map.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
+-- map.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
+-- map.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
 
 map.set("n", "D", function()
-  vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
 end, { desc = "diagnostic hover" })
 
 map.set("n", "<leader>q", ":qa!<CR>", { desc = "Save all and [q]uit" })
 
 map.set("n", "<c-q>", function()
-  vim.cmd("close") -- Save changes (if any)
+	vim.cmd("close") -- Save changes (if any)
 end, { desc = "Close split or open file explorer if last window" })
 
 map.set("n", "<up>", function()
-  local win_count = vim.fn.winnr("$") -- Get the total number of windows
-  if win_count > 1 then
-    vim.cmd("write")                 -- Save changes (if any)
-    pcall(vim.cmd, "close")          -- Safely close the current window (ignore errors)
-  else
-    vim.cmd("write")                 -- Save changes (if any)
-  end
+	local win_count = vim.fn.winnr("$") -- Get the total number of windows
+	if win_count > 1 then
+		vim.cmd("write") -- Save changes (if any)
+		pcall(vim.cmd, "close") -- Safely close the current window (ignore errors)
+	else
+		vim.cmd("write") -- Save changes (if any)
+	end
 end, { desc = "Close split or open file explorer if last window" })
 map.set("n", "<right>", "<C-w>v", { desc = "Split window vertically" })
 map.set("n", "<down>", "<C-w>s", { desc = "Split window horizontally" })
@@ -68,99 +68,99 @@ map.set("n", "<leader>lz", ":Lazy<CR>")
 
 -- NOTE: here goes custom mappins for creating new external documents
 vim.api.nvim_create_user_command("CreateNewNote", function()
-  -- Prompt for a custom filename
-  local filename = vim.fn.input("Enter note name:", "", "file")
+	-- Prompt for a custom filename
+	local filename = vim.fn.input("Enter note name:", "", "file")
 
-  -- Ensure the filename has a .md extension
-  if not filename:match("%.md$") then
-    filename = filename .. ".md"
-  end
+	-- Ensure the filename has a .md extension
+	if not filename:match("%.md$") then
+		filename = filename .. ".md"
+	end
 
-  local filepath = "~/notes/" .. filename
-  local expanded_filepath = vim.fn.expand(filepath)
+	local filepath = "~/notes/" .. filename
+	local expanded_filepath = vim.fn.expand(filepath)
 
-  -- Ensure directory exists
-  vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
+	-- Ensure directory exists
+	vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
 
-  -- Check if file exists
-  if vim.fn.filereadable(expanded_filepath) == 1 then
-    -- If the file exists, just open it
-    vim.cmd("edit " .. filepath)
-  else
-    -- If the file doesn't exist, create it
-    vim.fn.writefile({}, expanded_filepath, "b")
-    vim.cmd("edit " .. filepath)
-    -- Add a default line to avoid empty buffer
-    vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
-  end
+	-- Check if file exists
+	if vim.fn.filereadable(expanded_filepath) == 1 then
+		-- If the file exists, just open it
+		vim.cmd("edit " .. filepath)
+	else
+		-- If the file doesn't exist, create it
+		vim.fn.writefile({}, expanded_filepath, "b")
+		vim.cmd("edit " .. filepath)
+		-- Add a default line to avoid empty buffer
+		vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
+	end
 
-  -- Move to end of buffer, insert two new lines, and start insert mode for both cases
-  vim.cmd("$ | put _ | put _ | startinsert")
+	-- Move to end of buffer, insert two new lines, and start insert mode for both cases
+	vim.cmd("$ | put _ | put _ | startinsert")
 end, {})
 
 map.set("n", "<leader>nn", ":CreateNewNote<CR>", { noremap = true, silent = true, desc = "[N]ew [N]ote" })
 
 -- NOTE: create new message
 vim.api.nvim_create_user_command("CreateNewMessage", function()
-  -- Prompt for a custom filename
-  local filename = vim.fn.input("Enter note name:", "", "file")
+	-- Prompt for a custom filename
+	local filename = vim.fn.input("Enter note name:", "", "file")
 
-  -- Ensure the filename has a .md extension
-  if not filename:match("%.md$") then
-    filename = filename .. ".md"
-  end
+	-- Ensure the filename has a .md extension
+	if not filename:match("%.md$") then
+		filename = filename .. ".md"
+	end
 
-  local filepath = "~/messages/" .. filename
-  local expanded_filepath = vim.fn.expand(filepath)
+	local filepath = "~/messages/" .. filename
+	local expanded_filepath = vim.fn.expand(filepath)
 
-  -- Ensure directory exists
-  vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
+	-- Ensure directory exists
+	vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
 
-  -- Check if file exists
-  if vim.fn.filereadable(expanded_filepath) == 1 then
-    -- If the file exists, just open it
-    vim.cmd("edit " .. filepath)
-  else
-    -- If the file doesn't exist, create it
-    vim.fn.writefile({}, expanded_filepath, "b")
-    vim.cmd("edit " .. filepath)
-    -- Add a default line to avoid empty buffer
-    vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
-  end
+	-- Check if file exists
+	if vim.fn.filereadable(expanded_filepath) == 1 then
+		-- If the file exists, just open it
+		vim.cmd("edit " .. filepath)
+	else
+		-- If the file doesn't exist, create it
+		vim.fn.writefile({}, expanded_filepath, "b")
+		vim.cmd("edit " .. filepath)
+		-- Add a default line to avoid empty buffer
+		vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
+	end
 
-  -- Move to end of buffer, insert two new lines, and start insert mode for both cases
-  vim.cmd("$ | put _ | put _ | startinsert")
+	-- Move to end of buffer, insert two new lines, and start insert mode for both cases
+	vim.cmd("$ | put _ | put _ | startinsert")
 end, {})
 
 map.set("n", "<leader>nm", ":CreateNewMessage<CR>", { noremap = true, silent = true, desc = "[N]ew [M]essage" })
 -- journal one
 vim.api.nvim_create_user_command("CreateJournal", function()
-  local filename = "~/journal/" .. vim.fn.strftime("%d-%m-%Y") .. ".md"
-  local expanded_filename = vim.fn.expand(filename)
+	local filename = "~/journal/" .. vim.fn.strftime("%d-%m-%Y") .. ".md"
+	local expanded_filename = vim.fn.expand(filename)
 
-  -- Ensure directory exists
-  vim.fn.mkdir(vim.fn.fnamemodify(expanded_filename, ":h"), "p")
+	-- Ensure directory exists
+	vim.fn.mkdir(vim.fn.fnamemodify(expanded_filename, ":h"), "p")
 
-  -- Check if file exists
-  if vim.fn.filereadable(expanded_filename) == 1 then
-    -- If the file exists, just open it
-    vim.cmd("edit " .. filename)
-  else
-    -- If the file doesn't exist, create it
-    vim.fn.writefile({}, expanded_filename, "b")
-    vim.cmd("edit " .. filename)
-    -- Add a default line to avoid empty buffer
-    vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
-  end
+	-- Check if file exists
+	if vim.fn.filereadable(expanded_filename) == 1 then
+		-- If the file exists, just open it
+		vim.cmd("edit " .. filename)
+	else
+		-- If the file doesn't exist, create it
+		vim.fn.writefile({}, expanded_filename, "b")
+		vim.cmd("edit " .. filename)
+		-- Add a default line to avoid empty buffer
+		vim.api.nvim_buf_set_lines(0, 0, -1, true, { "# New Note" })
+	end
 
-  -- Move to end of buffer, insert two new lines, and start insert mode for both cases
-  vim.cmd("$ | put _ | put _ | startinsert")
+	-- Move to end of buffer, insert two new lines, and start insert mode for both cases
+	vim.cmd("$ | put _ | put _ | startinsert")
 end, {})
 
 map.set("n", "<leader>nj", ":CreateJournal<CR>", { desc = "[N]ew [J]ournal note" })
 
 local function toggle_spell()
-  vim.wo.spell = not vim.wo.spell -- Toggle spell checking for the current window
+	vim.wo.spell = not vim.wo.spell -- Toggle spell checking for the current window
 end
 
 map.set("n", "<leader>S", toggle_spell, { desc = "[S]pell check" })
@@ -174,22 +174,6 @@ map.set("n", "<leader>nh", "<cmd>Noice<cr>", { desc = "Notification History" })
 -- State variable to store the last buffer number
 local last_buffer = nil
 
--- Function to toggle between current and last buffer
-local function toggle_buffer()
-  local current_buffer = vim.api.nvim_get_current_buf()
-
-  if last_buffer == nil or last_buffer == current_buffer then
-    -- If no last buffer or same as current, jump to the previous buffer
-    vim.cmd("b#") -- Switch to the alternate buffer (last visited)
-    last_buffer = current_buffer
-  else
-    -- Switch back to the last saved buffer
-    vim.api.nvim_set_current_buf(last_buffer)
-    last_buffer = current_buffer
-  end
-end
-
-map.set("n", "<leader>j", toggle_buffer, { noremap = true, silent = true })
 map.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true, desc = "Show hover information" })
 
 map.set({ "n", "v" }, "gu", "~", { noremap = true, silent = true, desc = "Toggle case" })
@@ -202,3 +186,4 @@ map.set({ "n", "v" }, "gu", "~", { noremap = true, silent = true, desc = "Toggle
 -- 	vim.cmd.wincmd("J")
 -- 	vim.api.nvim_win_set_height(0, 10)
 -- end, { desc = "Toggle terminal" })
+map.set("n", "<leader>j", ":b#<CR>", { noremap = true, silent = true })
