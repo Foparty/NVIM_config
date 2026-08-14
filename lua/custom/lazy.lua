631b44b9bc05f4e610d2d9ce4ac05f27f9f09063
fo <103907142+Foparty@users.noreply.github.com>
2026-08-14 12:56:17 +0700

    theme

--- lua/custom/lazy.lua
+++ lua/custom/lazy.lua
@@ -48,6 +48,7 @@ require("lazy").setup({
 
 -- vim.cmd([[colorscheme vesper]])
 vim.cmd([[colorscheme nightfox]])
+-- vim.cmd([[colorscheme ayu]])
 -- vim.cmd([[colorscheme tokyonight]])
 -- vim.cmd([[colorscheme github_dark_high_contrast]])
 -- vim.cmd([[colorscheme gruvbox]])
