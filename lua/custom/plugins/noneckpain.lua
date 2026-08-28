8e935d388063c701ace22c19c0943a48eeb430e1
fo <103907142+Foparty@users.noreply.github.com>
2026-08-29 00:31:26 +0700

    width fixed

--- lua/custom/plugins/noneckpain.lua
+++ lua/custom/plugins/noneckpain.lua
@@ -1,14 +1,14 @@
 return {
-  "shortcuts/no-neck-pain.nvim",
-  keys = {
-    {
-      "<leader>cc",
-      "<cmd>NoNeckPain<cr>",
-      desc = "Centre buffer",
-    },
-  },
-  opts = {
-    width = 100,
-    noremap = true,
-  },
+	"shortcuts/no-neck-pain.nvim",
+	keys = {
+		{
+			"<leader>cc",
+			"<cmd>NoNeckPain<cr>",
+			desc = "Centre buffer",
+		},
+	},
+	opts = {
+		width = 120,
+		noremap = true,
+	},
 }
