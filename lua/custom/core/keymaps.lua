27072b9b643ee7fc5b342d1cacd4cdf3e0d67b9a
fo <103907142+Foparty@users.noreply.github.com>
2026-09-08 13:12:59 +0700

    ctrl+t thoughts

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -2,6 +2,9 @@ local map = vim.keymap
 
 map.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
+-- Free <C-t> for outer terminal (e.g. fzf); default is tag-stack pop
+-- map.set("n", "<C-t>", "<Nop>", { desc = "Unbind tag pop" })
+
 map.set("n", ";", ":", { desc = "Enter command mode" })
 
 map.set("i", "kj", "<Esc>:w<CR>", { desc = "Exit insert mode and save file" })
@@ -35,17 +38,17 @@ map.set("n", "<C-up>", "<C-w>+", { desc = "Increase window height" })
 map.set("n", "<C-down>", "<C-w>-", { desc = "Decrease window height" })
 
 map.set("n", "<leader>e", function()
-  vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
 end, { desc = "Open diagnostic float" })
 
 map.set("n", "<leader>q", ":wqa<CR>", { desc = "Save all and [q]uit" })
 
 map.set("n", "<c-q>", function()
-  vim.cmd("close")
+	vim.cmd("close")
 end, { desc = "Close split" })
 
 map.set("n", "<up>", function()
-  vim.cmd("close")
+	vim.cmd("close")
 end, { desc = "Close split" })
 map.set("n", "<right>", "<C-w>v", { desc = "Split window vertically" })
 map.set("n", "<down>", "<C-w>s", { desc = "Split window horizontally" })
@@ -59,61 +62,61 @@ map.set("n", "<leader>so", ":so %<CR>", { desc = "Source current file" })
 map.set("n", "<leader>lz", ":Lazy<CR>")
 
 local function open_or_create_doc(opts)
-  local filepath = opts.filepath
-  local default_lines = opts.default_lines or { "# New Note" }
-  local expanded_filepath = vim.fn.expand(filepath)
+	local filepath = opts.filepath
+	local default_lines = opts.default_lines or { "# New Note" }
+	local expanded_filepath = vim.fn.expand(filepath)
 
-  vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
+	vim.fn.mkdir(vim.fn.fnamemodify(expanded_filepath, ":h"), "p")
 
-  if vim.fn.filereadable(expanded_filepath) == 1 then
-    vim.cmd("edit " .. filepath)
-  else
-    vim.fn.writefile({}, expanded_filepath, "b")
-    vim.cmd("edit " .. filepath)
-    vim.api.nvim_buf_set_lines(0, 0, -1, true, default_lines)
-  end
+	if vim.fn.filereadable(expanded_filepath) == 1 then
+		vim.cmd("edit " .. filepath)
+	else
+		vim.fn.writefile({}, expanded_filepath, "b")
+		vim.cmd("edit " .. filepath)
+		vim.api.nvim_buf_set_lines(0, 0, -1, true, default_lines)
+	end
 
-  vim.cmd("$ | put _ | put _ | startinsert")
+	vim.cmd("$ | put _ | put _ | startinsert")
 end
 
 vim.api.nvim_create_user_command("CreateNewNote", function()
-  local filename = vim.fn.input("Enter note name:", "", "file")
-  if not filename:match("%.md$") then
-    filename = filename .. ".md"
-  end
-  open_or_create_doc({
-    filepath = "~/notes/" .. filename,
-    default_lines = { "# New Note" },
-  })
+	local filename = vim.fn.input("Enter note name:", "", "file")
+	if not filename:match("%.md$") then
+		filename = filename .. ".md"
+	end
+	open_or_create_doc({
+		filepath = "~/notes/" .. filename,
+		default_lines = { "# New Note" },
+	})
 end, {})
 
 map.set("n", "<leader>nn", ":CreateNewNote<CR>", { noremap = true, silent = true, desc = "[N]ew [N]ote" })
 
 vim.api.nvim_create_user_command("CreateNewMessage", function()
-  local filename = vim.fn.input("Enter note name:", "", "file")
-  if not filename:match("%.md$") then
-    filename = filename .. ".md"
-  end
-  open_or_create_doc({
-    filepath = "~/messages/" .. filename,
-    default_lines = { "# New Message" },
-  })
+	local filename = vim.fn.input("Enter note name:", "", "file")
+	if not filename:match("%.md$") then
+		filename = filename .. ".md"
+	end
+	open_or_create_doc({
+		filepath = "~/messages/" .. filename,
+		default_lines = { "# New Message" },
+	})
 end, {})
 
 map.set("n", "<leader>nm", ":CreateNewMessage<CR>", { noremap = true, silent = true, desc = "[N]ew [M]essage" })
 
 vim.api.nvim_create_user_command("CreateJournal", function()
-  local date = vim.fn.strftime("%d-%m-%Y")
-  open_or_create_doc({
-    filepath = "~/journal/" .. date .. ".md",
-    default_lines = { "# Journal " .. date },
-  })
+	local date = vim.fn.strftime("%d-%m-%Y")
+	open_or_create_doc({
+		filepath = "~/journal/" .. date .. ".md",
+		default_lines = { "# Journal " .. date },
+	})
 end, {})
 
 map.set("n", "<leader>nj", ":CreateJournal<CR>", { desc = "[N]ew [J]ournal note" })
 
 local function toggle_spell()
-  vim.wo.spell = not vim.wo.spell
+	vim.wo.spell = not vim.wo.spell
 end
 
 map.set("n", "<leader>S", toggle_spell, { desc = "[S]pell check" })
